9bb3f505196df8c0ee14a55e17ae345f08f5cdef
Malte Willmes <malte.willmes@gmail.com>
2021-04-07 21:41:37 -0700

    more small fixes

--- R/chinook.R
+++ R/chinook.R
@@ -5,13 +5,12 @@
 #' @docType data
 #' @usage data(chinook)
 #' @format An object of class \code{"data.frame"}
-#' #' \describe{
+#'  \describe{
 #'  \item{year}{A value between 1967 and 2019}
 #'  \item{Run}{Run type}
 #'  \item{Escapement}{Total escapement estimate}
 #' @references California Department of Fish and Wildlife, California Central Valley Chinook Population Database Report,GrandTab 2020.05.22
 #' @examples
-#'
 #' data(chinook)
 #' head(chinook)
 #'
