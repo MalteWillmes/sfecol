e861c8d021aaf899ae444294c2c0babe7031ad56
Malte Willmes <malte.willmes@gmail.com>
2021-04-07 20:49:04 -0700

    added CDFW grandtab example data

---
+++ R/chinook.R
@@ -0,0 +1,20 @@
+#' @title Chinook Salmon Escapement estimates for California's Central Valley
+#'
+#' A simplified version of the Chinook Salmon Escapement estimates for California's Central Valley for winter, spring, fall, and late-fall run.
+#' Total escapement includes hatchery and in-river fish.
+#' @docType data
+#' @usage data(chinook)
+#' @format An object of class \code{"data.frame"}
+#' #' \describe{
+#'  \item{year}{A value between 1967 and 2019}
+#'  \item{winter}{Total escapement estimate}
+#'  \item{spring}{Total escapement estimate}
+#'  \item{fall}{Total escapement estimate}
+#'  \item{late-fall}{Total escapement estimate}
+#' @references California Department of Fish and Wildlife, California Central Valley Chinook Population Database Report,GrandTab 2020.05.22
+#' @examples
+#'
+#' data(chinook)
+#' head(chinook)
+#'
+"chinook"
